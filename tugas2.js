83cb5ad488303cb01007425da94974006bc42937
as <adbudi1@gmail.com>
2021-06-27 11:39:10 +0700

    tugas 2 js
    tugas ini di kumpulkan untuk niomic

---
+++ tugas2.js
@@ -0,0 +1,22 @@
+var angmin = -5;
+var angka = 60;
+var angdes = 5.5;
+
+var kosong = 'ada';
+var undefined;
+kosong = null;
+
+var tinggi = false;
+var cm = 90;
+if (cm < 170)
+tinggi = true;
+else {
+  tinggi= false;
+}
+
+var array = [4,6,8,10,11]
+console.log(angdes);
+console.log(tinggi);
+console.log(kosong);
+console.log(undefined);
+console.log(array[4]);
